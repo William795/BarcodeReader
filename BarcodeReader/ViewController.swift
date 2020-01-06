1259f5d11a8b6ccbd1a82eae2f50565d6d8a92eb
William Moody <oswin795@outlook.com>
2020-01-06 15:31:31 -0700

    first commit

---
+++ BarcodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeReader
+//
+//  Created by William Moody on 1/6/20.
+//  Copyright © 2020 William Moody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
